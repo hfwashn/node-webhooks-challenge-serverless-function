b165716699f223b32f7a0303d39362161efb795a
hfwashn <74775537+hfwashn@users.noreply.github.com>
2023-12-27 15:43:35 -0500

    Update nylas.js

--- api/webhooks/nylas.js
+++ api/webhooks/nylas.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
+
 // example callback_url: https://node-webhooks-challenge-serverless-function.vercel.app/api/webhooks/nylas
-export default function handler(request, response) {
+export default  async function handler(request, response) {
   
   // /api/webhooks/nylas?challenge={{CHALLENGE_STRING}}
   if (request.method === "GET" && request.query.challenge) {
@@ -11,11 +13,27 @@ export default function handler(request, response) {
   }
 
  if (request.method === "POST") {
-   console.log('==========Message updated start==========');
-   request.body.deltas.map(deltas => console.log(JSON.stringify(deltas)));
-   console.log('==========Message updated end==========\n');
-   // Responding to Nylas is important to prevent the webhook from retrying
-   return response.status(200).end();
- }
+  console.log('==========Message updated start==========');
+  request.body.deltas.map(deltas => console.log(JSON.stringify(deltas)));
+  console.log('==========Message updated end==========\n');
 
+  // Send the data to Customer.io webhook endpoint
+  const customerIoWebhookUrl = 'https://api.customer.io/v1/webhook/f13fe1e2c48f1fd9';
+  
+  try {
+    // Assuming request.body contains the data you want to send to Customer.io
+    const eventData = request.body;
+
+    // Make an HTTP POST request to Customer.io webhook endpoint
+    await axios.post(customerIoWebhookUrl, eventData);
+
+    // Responding to Nylas is important to prevent the webhook from retrying
+    return response.status(200).end();
+  } catch (error) {
+    console.error('Error sending data to Customer.io:', error);
+
+    // Respond with an error status if the request to Customer.io fails
+    return response.status(500).end();
+  }
+}
 }
